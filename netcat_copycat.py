704b987566825a6e9195911632332f76127a935c
ottoalves22 <ottoalves22@gmail.com>
2023-05-25 14:52:12 -0400

    added NetCat class and run func

--- netcat_copycat.py
+++ netcat_copycat.py
@@ -20,21 +20,32 @@ def execute(cmd):
     return output.decode()
 
 class NetCat:
-    pass
+    def __init__(self, args, buffer=None) -> None:
+        self.args = args
+        self.buffer = buffer
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    def run(self):
+        if self.args.listen:
+            self.listen()
+        else:
+            self.send()
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='BHP Net Tool',
                                      formatter_class=argparse.RawDescriptionHelpFormatter,
                                      epilog=textwrap.dedent('''Example: 2
-                                                            netcat.py -t 192.168.1.108 -p 5555 -l -c #
+                                                            netcat_copycat.py -t 192.168.1.108 -p 5555 -l -c #
                                                             command shell
-                                                            netcat.py -t 192.168.1.108 -p 5555 -l -
+                                                            netcat_copycat.py -t 192.168.1.108 -p 5555 -l -
                                                             u=mytest.txt # upload to file
-                                                            netcat.py -t 192.168.1.108 -p 5555 -l -e=\"cat
+                                                            netcat_copycat.py -t 192.168.1.108 -p 5555 -l -e=\"cat
                                                             /etc/passwd\" # execute command
-                                                            echo 'ABC' | ./netcat.py -t 192.168.1.108 -p 135
+                                                            echo 'ABC' | ./netcat_copycat.py -t 192.168.1.108 -p 135
                                                             # echo text to server port 135
-                                                            netcat.py -t 192.168.1.108 -p 5555 # connect to
+                                                            netcat_copycat.py -t 192.168.1.108 -p 5555 # connect to
                                                             server
                                                             '''))
     parser.add_argument('-c', '--comand',
@@ -43,7 +54,7 @@ if __name__ == '__main__':
                         help='execute specified command')
     parser.add_argument('-l', '--listen',
                         action='store_true', help='listen')
-    parser.add_argument('-p', '--port', type='int', 
+    parser.add_argument('-p', '--port', type=int, 
                         default=5555, help='specified port')
     parser.add_argument('-t', '--target', default='192.168.1.203', 
                         help='specified IP')
