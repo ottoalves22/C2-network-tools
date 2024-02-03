3ffdca03a67c7ee85cb14362b255ca8ed641cb2e
ottoalves22 <ottoalves22@gmail.com>
2024-02-03 17:00:37 -0300

    added main func

--- tcp_udp_scripts/tcp_proxy.py
+++ tcp_udp_scripts/tcp_proxy.py
@@ -25,3 +25,24 @@ def server_loop(local_host,local_port,remote_host,remote_port,receive_first):
         proxy_thread = threading.Thread(target=proxy_handler, args=(client_socket,remote_host,remote_port,receive_first))
         proxy_thread.start()
 
+def main():
+# no fancy command-line parsing here
+    if len(sys.argv[1:]) != 5:
+        print("Usage: ./proxy.py [localhost] [localport] [remotehost] [remoteport] [receive_first]")
+        print("Example: ./proxy.py 127.0.0.1 9000 10.12.132.1 9000 True")
+        sys.exit(0)
+    # setup local listening parameters
+    local_host = sys.argv[1]
+    local_port = int(sys.argv[2])
+    # setup remote target
+    remote_host = sys.argv[3]
+    remote_port = int(sys.argv[4])
+    # this tells our proxy to connect and receive data
+    # before sending to the remote host
+    receive_first = sys.argv[5]
+    if "True" in receive_first:
+        receive_first = True
+    else:
+        receive_first = False
+    # now spin up our listening socket
+    server_loop(local_host,local_port,remote_host,remote_port,receive_first)
